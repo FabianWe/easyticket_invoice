dbbc4f695e026d6a777db2954f0bb3a7d93650b7
Fabian Wenzelmann <fabianwenzelmann@posteo.de>
2019-08-11 01:42:58 +0200

    invoice class

--- easyticket_invoice/invoice.py
+++ easyticket_invoice/invoice.py
@@ -15,32 +15,45 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Provides a class that represents an invoice and abstract classes for generating pdf invoices (implementations
-of these can be found in other packages).
+"""Provides a class that represents an invoice and abstract classes for generating pdf invoices.
+Also includes an implementation based on WeasyPrint and Django templates.
 """
 
 
 from decimal import Decimal
+from collections import defaultdict
 
 
 class Article(object):
     """
     An article in a an invoice.
 
+    An article has a unique id (usually int or str), a name and a description.
+    It also has a price that consists of three components (that must be given when the article is initialized):
+    net, tax and gross (where gross should be net + tax).
+    Just to be sure everything is okay this values must all be given and are not computed automatically.
+    But there are some helper functions to compute those values: compute_gross and compute_taxes.
+
+    An article also has a tax category that is used in an invoice to group together articles with the same tax rate.
+    This is just a string identifier, for example for a tax of 19% the identifier could be just '19%'.
+
     Attributes:
         article_id: Id of the article, usually int or str.
         name (str): Name of the article.
-        price (int): Price of the article in cents.
+        tax_category (str): Identifier for a tax category.
+        net (Decimal): The net value of this article.
+        tax (Decimal): The taxes included in this article.
+        gross (Decimal): The gross value of this article.
         description (str): Optional description of an article.
-        tax (int or decimal.Decimal or None): If not None represents the tax applied for the article, for example for
-            19% tax you can use the int 19 or the decimal (not float!) Decimal(0.19).
     """
-    def __init__(self, article_id, name, price, description='', tax=None):
+    def __init__(self, article_id, name, tax_category, net, tax, gross, description=''):
         self.article_id = article_id
         self.name = name
-        self.description = description
-        self.price = price
+        self.tax_category = tax_category
+        self.net = net
         self.tax = tax
+        self.gross = gross
+        self.description = description
 
 
 def compute_gross(net, tax_rate, quantize=Decimal('0.01')):
@@ -98,3 +111,91 @@ def compute_taxes(gross, tax_rate, quantize=Decimal('0.01')):
         return gross, taxes, gross
     net = gross / (Decimal('1') + tax_rate)
     return compute_gross(net, tax_rate, quantize)
+
+
+class Address(object):
+    """Represents an address of a person (issuer or recipient of an invoice).
+
+    Attributes:
+        first_name (str): First name.
+        last_name (str): Last name.
+        street (str): Street name without street number.
+        street_number (str): Street number as a string (for example '7A').
+        postcode (str): Postcode as a string.
+        location (str): City / location.
+        additional (str): Additional address line.
+        phone (str): Phone number.
+        mail (str): E-Mail address.
+    """
+    def __init__(self, first_name='', last_name='', street='', street_number='', postcode='', location='', additional='', phone='', mail=''):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.street = street
+        self.street_number = street_number
+        self.postcode = postcode
+        self.location = location
+        self.additional = additional
+        self.phone = phone
+        self.mail = mail
+
+
+class Invoice(object):
+    """Class representing an invoice.
+
+    The invoice contains general information (such as issuer and recipient addresses) as well as information about
+    articles and taxes.
+
+    Taxes are grouped in to so called tax categories. For example if you have two different taxes on an invoice, one
+    with a tax rate of 19% and one with 7% (German VAT) you could just add two categories, one with name '19%' and value
+    Decimal(0.190) and one with name '7%' and value Decimal('0.070'). I've added a leading zero just to be more accurate
+    when computing gross values and rounding them afterwards.
+    Each article added to an invoice is associated with such a tax group. Before adding an article you should ensure
+    that the tax category has been added before (see add_tax_category).
+
+    The articles contained in this invoice are stored in a dictionary mapping the item identifier (usually int or str)
+    to a list of articles.
+    That is each article ID can occurr multiple times in the invoice, for example with a different description
+    (seating information in the description etc.). Articles can be added with the add_article method.
+
+
+    Attributes:
+        issuer (Address): Address of the person that issued the invoice.
+        recipient (Address): Address of the recipient of the invoice.
+        date (datetime.date): Date the invoice was issued.
+        service_date (datetime.date or None): Date of the service / sell / delivery.
+        payment_information (list of str): List of information about how the debt has to be paid.
+        tax_categories (dict of str to Decimal): Information about the tax categories, see above.
+        articles (defaultdict of int / str to Article): Information about articles in this invoice, see above.
+    """
+    def __init__(self, issuer, recipient, date, payment_information, service_date=None, tax_categories=None):
+        self.articles = defaultdict(list)
+        if tax_categories is None:
+            tax_categories = dict()
+        self.tax_categories = tax_categories
+        self.issuer = issuer
+        self.recipient = recipient
+        self.date = date
+        self.service_date = service_date
+        self.payment_information = payment_information
+
+    def add_tax_category(self, name, tax_rate):
+        """Add a new tax category to the invoice.
+
+        Args:
+            name (str): The identifier of the category.
+            tax_rate (Decimal): The tax rate of this category.
+        """
+        self.tax_categories[name] = tax_rate
+
+    def add_article(self, article):
+        """Add an article to the articles dictionary.
+
+        Args:
+            article (Article): The article to add.
+
+        Raises:
+            KeyError: If the tax category of the article is not registered in the tax_categories dict.
+        """
+        if article.tax_category not in self.tax_categories:
+            raise KeyError('Tax category "%s" is not registered in this invoice' % str(article.tax_category))
+        self.articles[article.article_id].append(article)
